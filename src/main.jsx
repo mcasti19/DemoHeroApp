d0dc988dfc650b69525f8dfbab5ba08ab50cf7f1
Moises Castillo <castdevel@gmail.com>
2024-05-21 18:52:48 -0400

    una vez mas usando el HashRouter a ver

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import { HeroesApp } from './HeroesApp.jsx';
 import './styles.css';
 
+// ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <HeroesApp />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// )
+
 ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <HeroesApp />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
+
